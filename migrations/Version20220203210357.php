6163b6e1109d16edcd1b63766015f94ce5ab70ff
valentin <valentin.denavaut@hotmail.fr>
2022-02-07 13:04:50 +0100

    MaJ

--- migrations/Version20220203210357.php
+++ migrations/Version20220203210357.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220128201422 extends AbstractMigration
+final class Version20220203210357 extends AbstractMigration
 {
     public function getDescription(): string
     {
